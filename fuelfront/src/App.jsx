444741b88f65eea1fe41f172e4af8e8eee1085ed
YESHABI <yeshanbi.samarakoon@gmail.com>
2024-06-03 04:46:35 +0530

    Added the products to the pages

--- fuelfront/src/App.jsx
+++ fuelfront/src/App.jsx
@@ -1,10 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
-import Category from "./Pages/Category/Category";
-import Product from "./Pages/Product/Product";
+import Category from "./Pages/Category";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header/Header";
-
+import clothingbanner from './assets/clothingbanner.png'
+import cosmeticsbanner from './assets/cosmeticsbanner.png'
+import electronicsbanner from './assets/electronicsbanner.png'
+import ProductN from "./Pages/Product";
+import bg from './assets/bg.png'
 
 export default function App() {
   return (
@@ -13,13 +16,13 @@ export default function App() {
       <Header/>
       <Routes>
           <Route path="/" element={<Home/>}/>
-          <Route path="/Sports" element={<Category/>}/>
-          <Route path="/Relaxation" element={<Category/>}/>
-          <Route path="/Natural energy" element={<Category/>}/>
-          <Route path="/product" element={<Product/>}/>
-            <Route path=":productId" element={<Product/>}/>\
+          <Route path="/Sports" element={<Category category={"Sports"} banner={bg}/>}/>
+          <Route path="/Relaxation" element={<Category category={"Organic"} banner={bg} />}/>
+          <Route path="/Natural energy" element={<Category category={"Relaxation drink"} banner={bg}/>}/>
+          <Route path="/product/:proId" element={<ProductN/>}/>
+            <Route path=":productId" element={<ProductN/>}/>
            { /*<Route path="/cart-page" element={<Cart/>}/>
-            <Route path="/login" element={<Login/>}/> */}
+            <Route path="/login" element={<Login/>}/> */} 
       </Routes>
       <Footer/>
     </BrowserRouter> 
--- fuelfront/src/Components/About.jsx
+++ fuelfront/src/Components/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { } from 'react-icons/tb' 
 
 const About = () => {
   return (
@@ -6,6 +7,19 @@ const About = () => {
       <div>
         <div>
           <h3> Unvelling Our Products's kwy features</h3>
+           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+               nulla pariatur.</p>
+              <div>
+                <div>
+                    <div>
+                        <ToTruck/>
+                    </div>
+                </div>
+              </div>
 
         </div>
       </div>
@@ -14,3 +28,4 @@ const About = () => {
 }
 
 export default About
+ 
